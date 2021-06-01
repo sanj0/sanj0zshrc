1d23afe8b5db0fcfdc6b8718650d78aee925c7d9
Malte <malte.dostal@gmail.com>
2021-06-01 17:54:55 +0200

    add install and update script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SANJ0ZSHRC="sanj0zshrc"
+INSTALLED_SZSHRC="$HOME/.sanj0zshrc"
+ZSHRC="$HOME/.zshrc"
+SOURCE_LINE="source $INSTALLED_SZSHRC"
+
+# copy sanj0zshrc to home dir
+cp SANJ0ZSHRC INSTALLED_SZSHRC
+
+if ! grep -q "^$SOURCE_LINE" "$ZSHRC"; then
+    echo $SOURCE_LINE >> $ZSHRC
+    source $ZSHRC
+fi
