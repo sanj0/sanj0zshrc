c1730603a9c07d574efbea16300cf6f939e719bc
Malte <malte.dostal@gmail.com>
2021-11-26 07:42:03 +0100

    add vimrc to collection and install script

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 SANJ0ZSHRC="sanj0zshrc"
+SANJ0VIM="sanj0.vim"
 INSTALLED_SZSHRC="$HOME/.sanj0zshrc"
+INSTALLED_SVIM="$HOME/.sanj0.vim"
 ZSHRC="$HOME/.zshrc"
+VIMRC="$HOME/.vimrc"
 SOURCE_LINE="source $INSTALLED_SZSHRC"
+SOURCE_LINE_VIMRC="so $INSTALLED_SVIM"
 
 # copy sanj0zshrc to home dir
 cp $SANJ0ZSHRC $INSTALLED_SZSHRC
+cp $SANJ0VIM $INSTALLED_SVIM
 
 if ! grep -q "^$SOURCE_LINE" "$ZSHRC"; then
     echo $SOURCE_LINE >> $ZSHRC
     source $ZSHRC
+fi
+if ! grep -q "^$SOURCE_LINEVIMRC" "$VIMRC"; then
+    echo $SOURCE_LINE_VIMRC >> $VIMRC
 fi
---
+++ sanj0.vim
@@ -0,0 +1,4 @@
+set autoindent
+set nu
+syntax on
+set spelllang=en_us,de_de
