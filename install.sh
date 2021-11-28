f4b1e3de8006dc0c4846dd262ce6af81877cd66a
Malte <malte.dostal@gmail.com>
2021-11-28 12:28:19 +0100

    add echos to install script

--- install.sh
+++ install.sh
@@ -8,14 +8,19 @@ VIMRC="$HOME/.vimrc"
 SOURCE_LINE="source $INSTALLED_SZSHRC"
 SOURCE_LINE_VIMRC="so $INSTALLED_SVIM"
 
+echo copying files to home dir...
 # copy sanj0zshrc to home dir
 cp $SANJ0ZSHRC $INSTALLED_SZSHRC
 cp $SANJ0VIM $INSTALLED_SVIM
 
+echo installing to .zshrc
 if ! grep -q "^$SOURCE_LINE" "$ZSHRC"; then
     echo $SOURCE_LINE >> $ZSHRC
     source $ZSHRC
 fi
+
+echo installing to .vimrc
 if ! grep -q "^$SOURCE_LINEVIMRC" "$VIMRC"; then
     echo $SOURCE_LINE_VIMRC >> $VIMRC
 fi
+echo done!
